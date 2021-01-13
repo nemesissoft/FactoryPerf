d4272ceac8da29cdef21ad70bf73f457ea7e103c
Michał Bryłka <michal.brylka@gmail.com>
2021-01-13 22:42:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace FactoryPerf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var b1 = SimpleFactory.GetDefault().Create("A100");
+            //var b2 = new GeneratedFactory().Create("A100");
+            //var b3 = GeneratedDictionaryFactory.GetDefault().Create("A100");
+
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
